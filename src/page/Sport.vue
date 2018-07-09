bef0f850841e9fa326031c72b0a4cf9ffff11db2
guo <guo565656@gmail.com>
2018-07-09 18:00:46 +0800

    no message

--- src/page/Sport.vue
+++ src/page/Sport.vue
@@ -17,31 +17,6 @@ export default {
 
   },
   components: {},
-  beforeCreate() {
-    console.log('beforeCreate')
-  },
-  created() {
-    console.log('created')
-
-  },
-  beforeMount() {
-    console.log('beforeMount')
-  },
-  mounted() {
-    console.log('mounted')
-  },
-  beforeUpdate() {
-    console.log('beforeUpdate')
-  },
-  updated() {
-    console.log('updated')
-  },
-  beforeDestroy() {
-    console.log('beforeDestroy')
-  },
-  destroyed() {
-    console.log('destroyed')
-  },
 }
 </script>
 
